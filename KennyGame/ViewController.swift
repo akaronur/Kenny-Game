378cfd4b90ffe95eee358b01d6af382283b41766
Onur <onur@onur-air>
2019-04-21 02:05:38 +0300

    Initial Commit

---
+++ KennyGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KennyGame
+//
+//  Created by Onur on 21.04.2019.
+//  Copyright © 2019 Onur Akar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
